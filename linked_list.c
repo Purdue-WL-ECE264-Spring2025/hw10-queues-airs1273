bff280c9775404aece3e5e257d3e04fb83dba34e
airs1273 <jocylee5@gmail.com>
2025-04-04 14:38:28 -0400

    linked_list_1

--- linked_list.c
+++ linked_list.c
@@ -2,22 +2,92 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value)
+{
+  // creates node with next = NULL and value = value
+  struct list_node *node = malloc(sizeof(struct list_node));
+  if (!node)
+  {
+    return NULL;
+  }
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value)
+{
+  // insert value at head
+  struct list_node *ptr = new_node(value);
+  ptr->next = list->head;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value)
+{
+  // insert value at tail
+  if (list->head == NULL)
+  {
+    return new_node(value);
+  }
+  struct list_node *ptr = list->head;
+  while ((ptr->next) != NULL)
+  {
+    ptr = ptr->next;
+  }
+  ptr->next = new_node(value);
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list)
+{
+  // remove value from head
+  struct list_node *ptr = list->head;
+  size_t value = ptr->value;
+  if (ptr == NULL)
+  {
+    return NULL;
+  }
+  list->head = ptr->next;
+  free(ptr);
+  return value;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list)
+{
+  // remove value from tail
+  struct list_node *ptr = list->head;
+  if (ptr == NULL)
+  {
+    return NULL;
+  }
+  while ((ptr->next) != NULL)
+  {
+    ptr = ptr->next;
+  }
+  size_t value = ptr->value;
+  free(ptr);
+  return value;
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list)
+{
+  // free linked list
+  struct list_node *ptr1 = list.head;
+  struct list_node *ptr2 = list.head;
+  while (ptr1->next != NULL)
+  {
+    ptr1 = ptr1->next;
+    free(ptr2);
+    ptr2 = ptr1;
+  }
+  free(ptr1);
+}
 
 // Utility function to help you debugging, do not modify
-void dump_list(FILE *fp, struct linked_list list) {
+void dump_list(FILE *fp, struct linked_list list)
+{
   fprintf(fp, "[ ");
-  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next) {
+  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next)
+  {
     fprintf(fp, "%zu ", cur->value);
   }
   fprintf(fp, "]\n");
--- queue.c
+++ queue.c
@@ -1,8 +1,19 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state)
+{
+    // inserts at the _head_
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q)
+{
+    // dequeue removes from the _tail_
+    return (struct game_state){0};
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start)
+{
+    // implement a BFS that determines the shortest moves to solve a Tiles game
+    return 0;
+}
